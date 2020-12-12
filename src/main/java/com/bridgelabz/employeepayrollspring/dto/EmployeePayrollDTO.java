63b8be8dc40cdc6136976dca627328d36d4561f2
unknown <divyacharaninabha@gmail.com>
2020-12-12 14:28:13 +0530

    Added: Rest Controller to demonstrate various http methods

---
+++ src/main/java/com/bridgelabz/employeepayrollspring/dto/EmployeePayrollDTO.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.employeepayrollspring.dto;
+
+public class EmployeePayrollDTO {
+
+	private String name;
+	private long salary;
+	
+	public EmployeePayrollDTO(String name, long salary) {
+		super();
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmployeePayrollDTO [name=" + name + ", salary=" + salary + "]";
+	}
+	
+	
+}
