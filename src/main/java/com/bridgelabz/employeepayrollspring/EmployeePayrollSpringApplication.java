f50055347746ab20002a42ea52d1866a3bdaed0c
unknown <divyacharaninabha@gmail.com>
2020-12-13 15:22:40 +0530

    Added: Logging using Lombok library added

--- src/main/java/com/bridgelabz/employeepayrollspring/EmployeePayrollSpringApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollspring/EmployeePayrollSpringApplication.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.employeepayrollspring;
 
+import java.util.logging.LogRecord;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+		log.info("Employee Payroll Application Started");
 	}
 
 }
